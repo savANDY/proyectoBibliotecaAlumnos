9d1d046b0c6761472c220d85aa945630c69bd2da
enaut agirre <enautirakasle@gmail.com>
2017-03-24 00:24:08 +0100

    Lanzadorea kendu

--- src/vista/prestamo/FormularioPrestamo.java
+++ src/vista/prestamo/FormularioPrestamo.java
@@ -17,19 +17,6 @@ public class FormularioPrestamo extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			FormularioPrestamo dialog = new FormularioPrestamo();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
 	 * Create the dialog.
 	 */
 	public FormularioPrestamo() {
