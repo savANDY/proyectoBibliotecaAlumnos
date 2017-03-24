e8191369844c3284db477e14c64535b0f3a45194
Itsaso <Itsaso@10.22.72.179>
2017-03-24 08:57:48 +0100

    FormularioPrestamos leihoa.

--- src/controlador/ControladorPrestamo.java
+++ src/controlador/ControladorPrestamo.java
@@ -1,7 +1,73 @@
 package controlador;
 
+
+import modelo.ModeloPrestamo;
+import vista.Principal;
+import vista.prestamo.FormularioPrestamo;
+import vista.prestamo.GestionPrestamo;
+
+
 public class ControladorPrestamo {
 
+	private ModeloPrestamo modeloPrestamo;
+	private GestionPrestamo gestionPrestamo;
+	private FormularioPrestamo formularioPrestamo;
+	private Principal principal;
+	
+	public GestionPrestamo getGestorPrestamo() {
+		return gestionPrestamo;
+	}
+
+	public void setGestorPrestamo(GestionPrestamo gestorPrestamo) {
+		this.gestionPrestamo = gestorPrestamo;
+	}
+
+	
+	public ModeloPrestamo getModeloPrestamo() {
+		return modeloPrestamo;
+	}
+
+	public void setModeloPrestamo(ModeloPrestamo modeloPrestamo) {
+		this.modeloPrestamo = modeloPrestamo;
+	}
+
+	public GestionPrestamo getGestionPrestamo() {
+		return gestionPrestamo;
+	}
+
+	public void setGestionPrestamo(GestionPrestamo gestionPrestamo) {
+		this.gestionPrestamo = gestionPrestamo;
+	}
+
+	public Principal getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(Principal principal) {
+		this.principal = principal;
+	}
+
+	public FormularioPrestamo getFormularioPrestamo() {
+		return formularioPrestamo;
+	}
+
+	public void setFormularioPrestamo(FormularioPrestamo formularioPrestamo) {
+		this.formularioPrestamo = formularioPrestamo;
+	}
+
+	public ControladorPrestamo() {
+		super();
+		
+		
+	}
+	
+	public void abrirGestionPrestamo() {
+		this.gestionPrestamo.setVisible(true);
+	}
+	
+	public void abrirFormularioPrestamo() {
+		this.formularioPrestamo.setVisible(true);
+	}
 	
 	
 }
